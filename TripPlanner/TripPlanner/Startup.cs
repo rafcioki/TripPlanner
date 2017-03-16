1779b7d4515f2f7cb56c3be65ee09566528ba14d
Rafal Ostrowski <oester200729@gmail.com>
2017-03-17 00:08:50 +0100

    Initial commit - empty MVC project

---
+++ TripPlanner/TripPlanner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TripPlanner.Startup))]
+namespace TripPlanner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
